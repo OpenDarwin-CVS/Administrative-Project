47d4c6732cee79c411d672cef1b2c6449462b0b2
kevin <kevin>
2002-05-23 05:31:53 +0000

    Initial revision

---
+++ usr/local/bin/od_wrapper.c
@@ -0,0 +1,63 @@
+/* Copyright (c)  2002, Apple Computer, Inc. All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+// 
+// 1.  Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 
+// 2.  Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+// 
+// 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of
+// its contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+extern char *sys_errorlist;
+extern int sys_nerr;
+
+ 
+int main(int argc, char* argv[], char* envp[] )
+{
+	char * file_to_run = "";
+	int option = atoi(argv[1]);
+	switch (option) {
+  	   case 1:
+		file_to_run = "/usr/local/bin/od_approve";
+		break;
+  	   case 2:
+		file_to_run = "/usr/local/bin/od_adduser";
+		break;
+	   case 3:
+		file_to_run = "/usr/local/bin/od_newproj";
+		break;
+	   case 4:
+		file_to_run = "/usr/local/bin/od_joingroup";
+		break;
+	   default:
+		exit -1;
+	}
+	execve(file_to_run, argv, envp);
+
+	perror(NULL);
+        return 0;
+}
+
